591a43b1ab1e58b5edca1a433589f8cfcc909c28
szewczukk <dmitro.szewczuk@gmail.com>
2025-03-07 17:55:07 +0100

    Adjust description field height

--- lib/sludge_web/live/streamer_live.ex
+++ lib/sludge_web/live/streamer_live.ex
@@ -27,27 +27,27 @@ defmodule SludgeWeb.StreamerLive do
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="flex gap-4 h-full">
-      <div class="flex-grow flex flex-col justify-between gap-4">
-        <div class="flex-1 flex flex-col border border-indigo-200 rounded-lg dark:border-zinc-800">
-          <div class="border-b border-indigo-200 px-8 py-4 dark:border-zinc-800 flex justify-between items-center gap-4">
+    <div class="flex gap-4">
+      <div class="flex-grow-0 flex flex-col justify-start gap-4">
+        <div class="flex-grow-0 flex flex-col border border-indigo-200 rounded-lg dark:border-zinc-800">
+          <div class="border-b border-indigo-200 px-8 py-2 flex justify-between items-center gap-4 dark:border-zinc-800">
             <h1 class="font-medium dark:text-neutral-200">
               Stream details
             </h1>
             <.dropping class="py-1">
-              <div class="flex items-center gap-2 text-sm">
+              <div class="flex items-center gap-2 text-xs">
                 <.icon name="hero-eye" class="w-4 h-4" />
                 {@viewers_count}
               </div>
             </.dropping>
           </div>
-          <form phx-submit="stream-config-update" class="flex flex-col items-stretch gap-2 flex-1 p-4">
+          <form phx-submit="stream-config-update" class="flex-1 flex flex-col items-stretch gap-2 p-4">
             <div class="flex gap-2">
               <input
                 type="text"
                 name="title"
                 placeholder="Title..."
-                class="rounded-lg border-indigo-200 text-sm flex-1 dark:bg-zinc-800 dark:border-none dark:text-indigo-400"
+                class="rounded-lg border-indigo-200 text-sm flex-1 dark:bg-zinc-800 dark:border-none dark:text-neutral-400"
               />
               <button class="rounded-lg bg-indigo-800 text-white py-3 px-16 text-sm hover:bg-indigo-900 self-start">
                 Save
@@ -56,7 +56,7 @@ defmodule SludgeWeb.StreamerLive do
             <textarea
               name="description"
               placeholder="Description..."
-              class="rounded-lg resize-none border-indigo-200 text-sm flex-1 dark:bg-zinc-800 dark:border-none dark:text-indigo-400"
+              class="flex-1 rounded-lg resize-none border-indigo-200 text-sm dark:bg-zinc-800 dark:border-none dark:text-neutral-400"
             />
           </form>
         </div>
