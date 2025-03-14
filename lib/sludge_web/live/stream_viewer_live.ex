434a212f1289f9fe41f7bf44cf0ee5c5cb573bcd
szewczukk <dmitro.szewczuk@gmail.com>
2025-03-14 13:17:38 +0100

    Break words

--- lib/sludge_web/live/stream_viewer_live.ex
+++ lib/sludge_web/live/stream_viewer_live.ex
@@ -21,7 +21,7 @@ defmodule SludgeWeb.StreamViewerLive do
             <%= if @stream_metadata.streaming? do %>
               <.live_dropping />
             <% end %>
-            <h1 class="text-2xl line-clamp-2 dark:text-neutral-200">
+            <h1 class="text-2xl line-clamp-2 dark:text-neutral-200 break-all">
               {if @stream_metadata, do: @stream_metadata.title, else: "The stream is offline"}
             </h1>
           </div>
@@ -45,7 +45,7 @@ defmodule SludgeWeb.StreamViewerLive do
           </div>
           <p
             :if={@stream_metadata.streaming?}
-            class="flex-grow overflow-y-scroll dark:text-neutral-400"
+            class="flex-grow overflow-y-scroll dark:text-neutral-400 break-all"
           >
             {@stream_metadata.description}
           </p>
